356769dcc6bc64ae92b734240c06ae17baa64e89
Simone Arpe <simon.arpe@gmail.com>
2018-11-26 18:58:15 +0100

    Add simple permission
    Signed-off-by: Simone Arpe <simon.arpe@gmail.com>

--- lib/pspdfkit.dart
+++ lib/pspdfkit.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:simple_permissions/simple_permissions.dart';
 
 class Pspdfkit {
   static const MethodChannel _channel = const MethodChannel('pspdfkit');
@@ -10,4 +11,10 @@ class Pspdfkit {
 
   static Future<void> present(String document) =>
     _channel.invokeMethod('present', <String, dynamic>{'document': document});
+
+  static Future<bool> checkWriteExternalStoragePermission() =>
+    SimplePermissions.checkPermission(Permission.WriteExternalStorage);
+
+  static Future<PermissionStatus> requestWriteExternalStoragePermission() =>
+    SimplePermissions.requestPermission(Permission.WriteExternalStorage);
 }
