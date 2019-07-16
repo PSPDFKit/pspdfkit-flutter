b429817bdcd3ee5b350cb6a27f18ac2708e131df
Rad Azzouz <rad@pspdfkit.com>
2019-07-16 11:32:07 -0400

    Fix custom title in the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -141,7 +141,7 @@ class _MyAppState extends State<MyApp> {
         iOSRightBarButtonItems:['thumbnailsButtonItem', 'activityButtonItem', 'searchButtonItem', 'annotationButtonItem'],
         iOSLeftBarButtonItems:['settingsButtonItem'],
         iOSAllowToolbarTitleChange: false,
-        toolbarTitle: null
+        toolbarTitle: 'Custom Title'
       });
     } on PlatformException catch (e) {
       print("Failed to open document: '${e.message}'.");
