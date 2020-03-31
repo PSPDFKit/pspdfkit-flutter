96c5f61ecd655f3be1c1abdb1ed8cba67ef82a4b
Akshat Patel <akshat.patel16@gmail.com>
2020-03-31 15:58:08 +0530

    Removed unneeded methods from header.

--- ios/Classes/PspdfkitFlutterHelper.h
+++ ios/Classes/PspdfkitFlutterHelper.h
@@ -27,21 +27,6 @@ NS_ASSUME_NONNULL_BEGIN
 + (void)setLeftBarButtonItems:(nullable NSArray <NSString *> *)items forViewController:(PSPDFViewController *)pdfViewController;
 + (void)setRightBarButtonItems:(nullable NSArray <NSString *> *)items forViewController:(PSPDFViewController *)pdfViewController;
 
-// Instant JSON Helpers
-+ (id)addAnnotation:(id)jsonAnnotation forViewController:(PSPDFViewController *)pdfViewController;
-+ (id)removeAnnotation:(id)jsonAnnotation forViewController:(PSPDFViewController *)pdfViewController;
-
-// Annotation Helpers
-+ (id)getAnnotationsForPageIndex:(PSPDFPageIndex)pageIndex andType:(NSString *)typeString forViewController:(PSPDFViewController *)pdfViewController;
-+ (id)getAllUnsavedAnnotationsForViewController:(PSPDFViewController *)pdfViewController;
-
-// Annotation processing Helper
-+ (id)processAnnotationsOfType:(NSString *)type withProcessingMode:(NSString *)processingMode andDestinationPath:(NSString *)destinationPath forViewController:(PSPDFViewController *)pdfViewController;
-
-// XFDF Helpers
-+ (id)importXFDFFromPath:(NSString *)path forViewController:(PSPDFViewController *)pdfViewController;
-+ (id)exportXFDFToPath:(NSString *)path forViewController:(PSPDFViewController *)pdfViewController;
-
 @end
 
 NS_ASSUME_NONNULL_END
