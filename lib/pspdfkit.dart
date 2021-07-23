526fefb3f5b3931f846984f1eac61841e6935b15
Imdat Solak <imdat@solak.de>
2021-07-23 10:04:27 +0200

    Added Configuration options and Android Permission Status to export

--- lib/pspdfkit.dart
+++ lib/pspdfkit.dart
@@ -2,4 +2,6 @@ library pspdfkit;
 
 export 'src/main.dart';
 export 'src/pspdfkit_view.dart';
+export 'src/configuration_options.dart';
+export 'src/android_permission_status.dart';
 export 'src/widgets/pspdfkit_widget.dart';
