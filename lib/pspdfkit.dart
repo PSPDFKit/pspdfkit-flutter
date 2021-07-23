00773568ae008d8ccd6d0f48a026c0ff029f76f6
Imdat Solak <imdat@solak.de>
2021-07-23 12:16:54 +0200

    Trying to fix widget issue for iOS

--- lib/pspdfkit.dart
+++ lib/pspdfkit.dart
@@ -1,7 +1,7 @@
 library pspdfkit;
 
 export 'src/main.dart';
-export 'src/pspdfkit_view.dart';
 export 'src/configuration_options.dart';
 export 'src/android_permission_status.dart';
-export 'src/widgets/pspdfkit_widget.dart';
+//export 'src/widgets/pspdfkit_widget.dart';
+//export 'src/pspdfkit_view.dart';
