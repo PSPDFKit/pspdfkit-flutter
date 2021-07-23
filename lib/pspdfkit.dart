90666456e59396c2e3d1de22a1cd911fc34d6373
Imdat Solak <imdat@solak.de>
2021-07-23 15:26:00 +0200

    Added View/Widget back

--- lib/pspdfkit.dart
+++ lib/pspdfkit.dart
@@ -3,5 +3,5 @@ library pspdfkit;
 export 'src/main.dart';
 export 'src/configuration_options.dart';
 export 'src/android_permission_status.dart';
-//export 'src/widgets/pspdfkit_widget.dart';
-//export 'src/pspdfkit_view.dart';
+export 'src/widgets/pspdfkit_widget.dart';
+export 'src/pspdfkit_view.dart';
